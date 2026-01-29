6f91f40c48a425f662e2a21ccf1b074f17148d03
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 00:44:21 +0530

    add src\components\layouts\index.js

---
+++ src/components/layouts/index.js
@@ -0,0 +1,7 @@
+export { default as AuthLayout } from './AuthLayout';
+export { default as MainLayout } from './MainLayout';
+export { default as AdminLayout } from './AdminLayout';
+export { default as InvestorLayout } from './InvestorLayout';
+export { default as AdvisorLayout } from './AdvisorLayout';
+export * from './Sidebar';
+EOF
