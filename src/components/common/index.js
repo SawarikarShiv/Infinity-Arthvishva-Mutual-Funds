4a55554a331acaf4916a7469d3984186cb54d4b2
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 00:42:46 +0530

    add src\components\common\index.js

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+export * from './Button';
+export * from './Card';
+export * from './Inputs';
+export * from './Modal';
+export * from './Table';
+export * from './Loader';
+export * from './Charts';
+export * from './Navigation';
+export * from './UI';
+EOF
