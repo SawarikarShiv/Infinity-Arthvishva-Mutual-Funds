563675d42317c851704aff024b98082b6501576f
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 00:49:58 +0530

    add src\assets\images\placeholder\index.js

---
+++ src/assets/images/placeholder/index.js
@@ -0,0 +1,3 @@
+export { default as AvatarPlaceholder } from './avatar.svg';
+export { default as FundPlaceholder } from './fund-placeholder.svg';
+export { default as ChartPlaceholder } from './chart-placeholder.svg';
