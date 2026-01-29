b3b1e7ba7683f582ee46b0e5545142a3f10eb27b
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 00:50:21 +0530

    add src\assets\illustrations\index.js

---
+++ src/assets/illustrations/index.js
@@ -0,0 +1,4 @@
+export { default as InvestmentIllustration } from './investment.svg';
+export { default as PortfolioGrowthIllustration } from './portfolio-growth.svg';
+export { default as RiskAnalysisIllustration } from './risk-analysis.svg';
+export { default as SIPCalculatorIllustration } from './sip-calculator.svg';
