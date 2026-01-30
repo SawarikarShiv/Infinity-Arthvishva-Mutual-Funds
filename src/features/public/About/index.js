536d72ce565e1f227e2e7de50b003ca464d7255e
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 22:33:37 +0530

    add src\features\public\About\index.js

---
+++ src/features/public/About/index.js
@@ -0,0 +1,4 @@
+export { default as AboutHero } from './components/AboutHero';
+export { default as TeamSection } from './components/TeamSection';
+export { default as ValuesSection } from './components/ValuesSection';
+EOF
