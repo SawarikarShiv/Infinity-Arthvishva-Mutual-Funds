bb511fc1676c226ad8c66551e3c45ee666bee6c7
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-30 16:56:42 +0530

    add index

---
+++ src/features/admin/Dashboard/index.js
@@ -0,0 +1,4 @@
+export { default as AdminOverview } from './components/AdminOverview';
+export { default as PlatformStats } from './components/PlatformStats';
+export { default as RecentActivities } from './components/RecentActivities';
+EOF
