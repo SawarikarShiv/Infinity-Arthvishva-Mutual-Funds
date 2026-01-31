a172ea55679bcd1ece4de5e49b281f47fd9944df
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-31 21:11:23 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-  ],
-  
-  // Base public path
-  base: '/',
+  plugins: [react()],
   
   // Development server configuration
   server: {
@@ -30,16 +20,6 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-      '/uploads': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    
-    // Watch configuration
-    watch: {
-      usePolling: true,
     },
   },
   
@@ -52,30 +32,18 @@ export default defineConfig({
   // Build configuration
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-          charts: ['recharts', 'chart.js', 'react-chartjs-2'],
-          state: ['@reduxjs/toolkit', 'react-redux', 'axios'],
-          utils: ['date-fns', 'i18next', 'react-i18next', 'yup'],
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'state-vendor': ['@reduxjs/toolkit', 'react-redux', 'axios'],
+          'ui-vendor': ['recharts', 'react-hook-form', 'yup'],
+          'utils-vendor': ['date-fns', 'i18next', 'react-i18next'],
         },
         chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: ({ name }) => {
-          if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')) {
-            return 'assets/images/[name]-[hash][extname]';
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/.test(name ?? '')) {
-            return 'assets/fonts/[name]-[hash][extname]';
-          }
-          if (/\.css$/.test(name ?? '')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        },
+        entryFileNames: 'assets/js/[entry]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash][extname]',
       },
     },
     // Minify configuration
@@ -87,7 +55,7 @@ export default defineConfig({
       },
     },
     // Chunk size warning limit
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 800,
   },
   
   // Resolve configuration
@@ -95,32 +63,23 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@features': path.resolve(__dirname, './src/features'),
       '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@features': path.resolve(__dirname, './src/features'),
       '@services': path.resolve(__dirname, './src/services'),
-      '@store': path.resolve(__dirname, './src/store'),
       '@utils': path.resolve(__dirname, './src/utils'),
-      '@config': path.resolve(__dirname, './src/config'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@styles': path.resolve(__dirname, './src/styles'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@store': path.resolve(__dirname, './src/store'),
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    extensions: ['.js', '.jsx', '.json'],
   },
   
   // CSS configuration
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @import "@styles/variables";
-          @import "@styles/mixins";
-        `,
+        additionalData: `@import "./src/styles/variables.scss";`,
       },
-    },
-    modules: {
-      localsConvention: 'camelCase',
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
   },
   
@@ -130,17 +89,10 @@ export default defineConfig({
       'react',
       'react-dom',
       'react-router-dom',
-      '@mui/material',
-      '@mui/icons-material',
-      '@emotion/react',
-      '@emotion/styled',
       '@reduxjs/toolkit',
       'react-redux',
       'axios',
-      'recharts',
-      'date-fns',
     ],
-    exclude: ['@mui/x-date-pickers'],
     esbuildOptions: {
       loader: {
         '.js': 'jsx',
@@ -148,19 +100,6 @@ export default defineConfig({
     },
   },
   
-  // Esbuild configuration
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  
   // Environment variables
   envPrefix: 'VITE_',
-  
-  // Log level
-  logLevel: 'info',
-  
-  // Clear screen
-  clearScreen: false,
 });
