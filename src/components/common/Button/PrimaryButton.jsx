aabe38127cb4dede0974cf3e0daeb1749a24dac2
Shivshankar <sawarikarshivshankar@gmail.com>
2026-01-31 11:46:49 +0530

    add src\components\common\Button\PrimaryButton.jsx

--- src/components/common/Button/PrimaryButton.jsx
+++ src/components/common/Button/PrimaryButton.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { clsx } from 'clsx';
+import clsx from '../../utils/clsx';
 
 const PrimaryButton = ({
   children,
-  onClick,
   type = 'button',
+  onClick,
   disabled = false,
   loading = false,
+  className = '',
   fullWidth = false,
   size = 'medium',
-  className = '',
   ...props
 }) => {
   const sizeClasses = {
     small: 'px-3 py-1.5 text-sm',
     medium: 'px-4 py-2.5 text-base',
-    large: 'px-6 py-3.5 text-lg',
+    large: 'px-6 py-3 text-lg',
   };
 
   return (
@@ -25,8 +24,8 @@ const PrimaryButton = ({
       onClick={onClick}
       disabled={disabled || loading}
       className={clsx(
-        'inline-flex items-center justify-center font-medium rounded-lg transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500',
-        'bg-blue-600 text-white hover:bg-blue-700 active:bg-blue-800',
+        'inline-flex items-center justify-center font-medium rounded-lg transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2',
+        'bg-blue-600 hover:bg-blue-700 text-white focus:ring-blue-500',
         'disabled:opacity-50 disabled:cursor-not-allowed',
         sizeClasses[size],
         fullWidth && 'w-full',
@@ -61,16 +60,4 @@ const PrimaryButton = ({
   );
 };
 
-PrimaryButton.propTypes = {
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func,
-  type: PropTypes.oneOf(['button', 'submit', 'reset']),
-  disabled: PropTypes.bool,
-  loading: PropTypes.bool,
-  fullWidth: PropTypes.bool,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  className: PropTypes.string,
-};
-
 export default PrimaryButton;
-EOF
