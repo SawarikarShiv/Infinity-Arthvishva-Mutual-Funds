4dd57b3cef45d99ad67d747b4a2b35aa703ae762
Shivshankar <sawarikarshivshankar@gmail.com>
2026-02-01 17:39:09 +0530

    update src\store\store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './rootReducer.js'
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import notificationReducer from './slices/notificationSlice'; // Add this line
+// ... import other reducers
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: authReducer,
+    notification: notificationReducer, // Add this line
+    // ... other reducers
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-  devTools: import.meta.env.DEV,
-})
+});
 
-export default store
+export default store;
