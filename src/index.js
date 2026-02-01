1a56589b33c7d6fdf5ad2e9f2202f55e05d70420
Shivshankar <sawarikarshivshankar@gmail.com>
2026-02-01 23:48:35 +0530

    update src\index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import App from './App';
-import { store } from './store';
+import { store } from "./store/store";
 import './styles/global.scss';
 
 // Disable React DevTools in production (without external package)
