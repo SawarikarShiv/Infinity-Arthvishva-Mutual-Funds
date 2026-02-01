a9a1db4b6b29b5d5d9dedebdc54d81338da32ba3
Shivshankar <sawarikarshivshankar@gmail.com>
2026-02-01 23:52:06 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// ES modules fix for __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   
